c4370b549a8a250149044e175eddba6a0a0b1e0d
Leon <liverpoolleon16@googlemail.com>
2026-06-15 19:17:26 +0100

    Added rate limiter to addresslist endpoint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            globals: { ...globals.node },
+        },
+        rules: {
+            'no-console': 'warn',
+            'no-var': 'error',
+            eqeqeq: ['error', 'always'],
+        },
+    },
+    {
+        files: ['tests/**/*.js'],
+        languageOptions: {
+            globals: { ...globals.jest },
+        },
+    },
+    {
+        ignores: ['node_modules/', 'public/'],
+    },
+    require('eslint-config-prettier'),
+];
