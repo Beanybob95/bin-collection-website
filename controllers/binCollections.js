beacc84309e4a15206c2e100b9322d01910a2842
Leon <liverpoolleon16@googlemail.com>
2026-06-16 11:05:10 +0100

    Made UI/UX improvments to the calendar screen

--- controllers/binCollections.js
+++ controllers/binCollections.js
@@ -2,6 +2,11 @@ const Address = require('../models/Address');
 const BinCollectionDates = require('../models/BinCollectionDates');
 const dbDebug = require('debug')('app:db');
 
+const typeLabels = {
+    res: 'Household waste',
+};
+const defaultTypeLabel = 'Recycling & Glass';
+
 module.exports.show = async (req, res) => {
     const address = await Address.findById(req.params.id);
     const binCollections = await BinCollectionDates.find({
@@ -11,9 +16,31 @@ module.exports.show = async (req, res) => {
     dbDebug(address);
     dbDebug(binCollections);
 
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const types = [...new Set(binCollections.map((c) => c.type))];
+    const nextCollections = types
+        .map((type) => {
+            const nextDate = binCollections
+                .filter((c) => c.type === type && c.date >= today)
+                .map((c) => c.date)
+                .sort((a, b) => a - b)[0];
+            return nextDate
+                ? {
+                      type,
+                      label: typeLabels[type] || defaultTypeLabel,
+                      date: nextDate,
+                  }
+                : null;
+        })
+        .filter(Boolean)
+        .sort((a, b) => a.date - b.date);
+
     res.render('bin-collections/show', {
         title: 'Collection Dates',
         address,
         binCollections,
+        nextCollections,
     });
 };
