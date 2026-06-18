e4ab114c09159b019147dac20895ba688c3c6d16
Leon <liverpoolleon16@googlemail.com>
2026-06-18 17:49:23 +0100

    Front end changes to replace lorem ipsum and other temp text

--- routes/auth.js
+++ routes/auth.js
@@ -13,12 +13,10 @@ const authLimiter = rateLimit({
     message: { error: 'Too many requests, please try again later.' },
 });
 
-router.use(authLimiter);
-
 router.get('/signup', authController.signupForm);
-router.post('/signup', authController.signup);
+router.post('/signup', authLimiter, authController.signup);
 router.get('/login', authController.loginForm);
-router.post('/login', authController.login);
+router.post('/login', authLimiter, authController.login);
 router.post('/logout', authController.logout);
 
 module.exports = router;
